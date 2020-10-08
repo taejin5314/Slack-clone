b3bebaf8448d67d740b59ad924a0b0f4e7dfcead
taejin <taejin5314@gmail.com>
2020-10-08 17:36:06 -0400

    Component: Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Initialize the app</h2>
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                {/* Avatars for logged in user */}
+                {/* Time Icon */}
+            </div>
+            <div className="header__search">
+                {/* Search Icon */}
+                {/* Input */}
+            </div>
+            <div className="header__right">
+                {/* Help Icon */}
+            </div>
+        </div>
+    )
+}
+
+export default Header
