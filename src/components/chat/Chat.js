9903f648a55fd98783ed9fd9171d7e82fbef398e
taejin <taejin5314@gmail.com>
2020-10-09 15:54:31 -0400

    Component: Chat CSS

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
     const { roomId } = useParams();
 
     return (
         <div className="chat">
-            <h2>You are in the {roomId} room</h2>
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong># general</strong>
+                        <StarBorderIcon />
+                    </h4>
+                </div>
+
+                <div className="chat__headerRight">
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
