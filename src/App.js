d6f2c1a587b461bc57b185fef4c8d4b293e79832
taejin <taejin5314@gmail.com>
2020-10-09 19:59:41 -0400

    App.js: no user -> login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
@@ -6,24 +6,32 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Chat from './components/chat/Chat';
 
 function App() {
+  const [User, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        {/* Header */}
-        <Header />
-        <div className="app__body">
-          {/* Sidebar */}
-          <Sidebar />
+        {!User ? (
+          <h1>LOGIN page</h1>
+        ) : (
+            <>
+              {/* Header */}
+              < Header />
+              <div className="app__body">
+                {/* Sidebar */}
+                <Sidebar />
 
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+                <Switch>
+                  <Route path="/room/:roomId">
+                    <Chat />
+                  </Route>
+                  <Route path="/">
+                    <h1>Welcome</h1>
+                  </Route>
+                </Switch>
+              </div>
+            </>
+          )}
       </Router>
     </div>
   );
